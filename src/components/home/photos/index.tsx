e7cb2e25139db171b78430b16b743b9ef85f2180
Manish Gotame <manishgt194@gmail.com>
2025-03-08 22:23:24 +0545

    file types converted

--- src/components/home/photos/index.tsx
+++ src/components/home/photos/index.tsx
@@ -10,22 +10,22 @@ const photos: IPhoto[] = [
   {
     title: 'Photo 1',
     description: 'Description 1',
-    image: '/photos/IMG_1.JPEG'
+    image: '/photos/IMG_1.jpg'
   },
   {
     title: 'Photo 2',
     description: 'Description 2',
-    image: '/photos/IMG_2.JPEG'
+    image: '/photos/IMG_2.jpg'
   },
   {
     title: 'Photo 3',
     description: 'Description 3',
-    image: '/photos/IMG_3.JPEG'
+    image: '/photos/IMG_3.jpg'
   },
   {
     title: 'Photo 4',
     description: 'Description 4',
-    image: '/photos/IMG_4.JPEG'
+    image: '/photos/IMG_4.jpg'
   }
 ]
 
