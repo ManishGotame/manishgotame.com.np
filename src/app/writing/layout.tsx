b2ace78d38dc8f5f77e676af5ec25e2986ac97ef
Manish Gotame <manishgt194@gmail.com>
2025-04-10 22:21:32 +0545

    writing section layout added

---
+++ src/app/writing/layout.tsx
@@ -0,0 +1,22 @@
+import { WritingLayout } from '@/components'
+import { getTitles } from '@/lib'
+import { NotionAPI } from 'notion-client'
+
+export const revalidate = 60
+
+export default async function Layout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  const notion = new NotionAPI()
+  const recordMap = await notion.getPage(process.env.NOTION_PAGE_ID as string)
+  const blockMap = recordMap.block
+  const articleTitles = getTitles(blockMap)
+
+  return (
+    <WritingLayout title='Writing' open={false} articleTitles={articleTitles}>
+      {children}
+    </WritingLayout>
+  )
+}
