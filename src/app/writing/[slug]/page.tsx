6f3c17d02a92e705f0975f6114004e34eea3b30d
Manish Gotame <manishgt194@gmail.com>
2025-04-14 23:13:18 +0545

    Fix: added missing not found text for writing

--- src/app/writing/[slug]/page.tsx
+++ src/app/writing/[slug]/page.tsx
@@ -1,17 +1,28 @@
 import { getBlockTitle } from 'notion-utils'
 import Post from './post'
 import { notion } from '@/lib'
+import { CloudAlertIcon } from 'lucide-react'
 
 type tParams = Promise<{ slug: string }>
 
 export const revalidate = 30
 
 export default async function Page({ params }: { params: tParams }) {
-  const { slug } = await params
-  const data = await notion.getPage(slug as string)
-  const keys = Object.keys(data?.block || {})
-  const block = data?.block?.[keys[0]]?.value
-  const title = getBlockTitle(block, data)
+  try {
+    const { slug } = await params
+    const data = await notion.getPage(slug as string)
 
-  return <Post blockMap={data} title={title} />
+    const keys = Object.keys(data?.block || {})
+    const block = data?.block?.[keys[0]]?.value
+    const title = getBlockTitle(block, data)
+
+    return <Post blockMap={data} title={title} />
+  } catch {
+    return (
+      <div className='flex min-h-screen items-center justify-center flex-col gap-2'>
+        <CloudAlertIcon className='w-10 h-10' />
+        <h1 className='text-xl font-medium'>Post not found</h1>
+      </div>
+    )
+  }
 }
